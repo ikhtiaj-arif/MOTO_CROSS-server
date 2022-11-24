a25d35238f3418d72990af0c0c65ef98c302bdd4
AnGon777 <ikhtiaj.arif@gmail.com>
2022-11-24 09:28:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Server is running...')
+  })
+
+  
+  
+  app.listen(port, () => {
+    console.log(`Server is running...on ${port}`)
+  })
+  
