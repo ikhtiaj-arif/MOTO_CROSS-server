837f7b4c8fd5074faf09d9f9b574435efcc27cef
AnGon777 <ikhtiaj.arif@gmail.com>
2022-11-28 00:47:43 +0600

    payment added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const jwt = require('jsonwebtoken')
 
 const app = express()
@@ -47,6 +48,34 @@ async function run() {
         const allBikesCollection = client.db('motocross').collection('allBikes')
         const bookingsCollection = client.db('motocross').collection('bookings')
         const reportsCollection = client.db('motocross').collection('reports')
+        const paymentsCollection = client.db('motocross').collection('payments')
+
+
+// payment intent
+        app.post('/create-payment-intent', async(req, res)=>{
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+           
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+               
+                  "payment_method_types": [
+                    "card"
+                  ]
+                
+              });
+              res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+        })
+// payments data
+        app.post('/payments', async(req, res)=>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment)
+            res.send(payment)
+        })
 
 // set or update user to database  
         app.put('/users/:email', async(req, res)=>{
@@ -222,6 +251,15 @@ async function run() {
             const result = await bookingsCollection.find(query).toArray();
             res.send(result)
         })
+// get booking with id
+        app.get('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingsCollection.findOne(query)
+            res.send(result)
+        })
+
+
 // post bookings with user info
         app.post('/bookings', async(req, res) => {
             const product = req.body;
